350fe037d1f50b8e8fb3548b2daf4c65a8aa077a
ausmith <goldsmithaaron@gmail.com>
2016-05-27 14:05:43 -0400

    Switch to using our API call for the data fetch

--- routes/resume.js
+++ routes/resume.js
@@ -1,27 +1,29 @@
 var express = require('express'),
-    fs = require('fs');
+    fs = require('fs'),
+    http = require('http');
 var router = express.Router();
 
-// TODO: Switch to API calls like originally planned
-var readFile = function(fname) {
-  return fs.readFileSync(fname, 'utf8', function(err, data) { return data; });
-};
-
-var fileName = 'resume.json';
-var fetchData = function(key) {
-  return JSON.parse(readFile(fileName))[key];
+var fetchAll = function(callback) {
+  http.get({
+    host: 'localhost',
+    port: 3000,
+    path: '/data'
+  }, function(response) {
+    var body = '';
+    response.on('data', function(d) { body += d; });
+    response.on('end', function() {
+      var parsed = JSON.parse(body);
+      return callback(parsed);
+    });
+  });
 };
 
 /* Display resume */
 router.get('/', function(req, res, next) {
-  var data = {
-    contact: fetchData('contact'),
-    toolbox: fetchData('toolbox'),
-    experience: fetchData('experience'),
-    activities: fetchData('activities'),
-    education: fetchData('education')
-  };
-  res.render('resume', data);
+  // TODO: make callback take an error node first
+  fetchAll(function(data) {
+    res.render('resume', data);
+  });
 });
 
 module.exports = router;
