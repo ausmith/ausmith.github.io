a5616106eb434f1c74afd450da1994032651ae4b
ausmith <goldsmithaaron@gmail.com>
2016-05-27 23:30:50 -0400

    Make routing more dynamic for data retrieval

--- routes/data.js
+++ routes/data.js
@@ -2,39 +2,50 @@ var express = require('express');
 var fs = require('fs');
 var router = express.Router();
 
-// TODO: Cache file data on first load
 var readFile = function(fname) {
   return fs.readFileSync(fname, 'utf8', function(err, data) { return data; });
 };
 
 var fileName = 'resume.json';
-var fetchData = function(key) {
-  return JSON.parse(readFile(fileName))[key];
-};
-
-/* Contact */
-router.get('/contact', function(req, res) {
-  res.json(fetchData('contact'));
+var resume = {};
+router.use(function (req, res, next) {
+  resume = JSON.parse(readFile(fileName));
+  next();
 });
 
-/* Education */
-router.get('/education', function(req, res) {
-  res.json(fetchData('education'));
-});
-
-/* Experience */
-router.get('/experience', function(req, res) {
-  res.json(fetchData('experience'));
-});
-
-/* Toolbox */
-router.get('/toolbox', function(req, res) {
-  res.json(fetchData('toolbox'));
-});
-
-/* All */
 router.get('/', function(req, res) {
-  res.json(JSON.parse(readFile(fileName)));
+  res.json(resume);
+});
+
+router.get('/:k1', function(req, res, next) {
+  var k1 = req.params.k1;
+  if(resume[k1] == undefined || resume[k1] == null) next('route');
+  else next();
+}, function(req, res) {
+  res.json(resume[req.params.k1]);
+});
+
+router.get('/:k1/:k2', function(req, res, next) {
+  var k1 = req.params.k1,
+      k2 = req.params.k2;
+  if(resume[k1] == undefined || resume[k1] == null) next('route');
+  else if(resume[k1][k2] == undefined || resume[k1][k2] == null) next('route');
+  else next();
+}, function(req, res) {
+  res.json(resume[req.params.k1][req.params.k2]);
+});
+
+
+router.get('/:k1/:k2/:k3', function(req, res, next) {
+  var k1 = req.params.k1,
+      k2 = req.params.k2,
+      k3 = req.params.k3;
+  if(resume[k1] == undefined || resume[k1] == null) next('route');
+  else if(resume[k1][k2] == undefined || resume[k1][k2] == null) next('route');
+  else if(resume[k1][k2][k3] == undefined || resume[k1][k2][k3] == null) next('route');
+  else next();
+}, function(req, res) {
+  res.json(resume[req.params.k1][req.params.k2][req.params.k3]);
 });
 
 module.exports = router;
